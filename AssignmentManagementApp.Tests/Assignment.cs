098c757979412cfe7695475595093d0870d73452
Connor Gaskill <gaskillconnor@gmail.com>
2025-04-13 16:13:12 -0700

    Added Assignment class. Made Assignment.Title required Implemented and passed test requiring Assignment.Title to be not null.

---
+++ AssignmentManagementApp.Tests/Assignment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentManagementApp.Tests
+{
+    public class Assignment
+    {
+        [Required(ErrorMessage = "Assignment title is required.")]
+        public string Title { get; set; }
+    }
+}
---
+++ AssignmentManagementApp.Tests/AssignmentTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace AssignmentManagementApp.Tests
+{
+    public class AssignmentTests
+    {
+        [Fact]
+        public void Assignment_Should_Have_a_Title()
+        {
+            var assignment = new Assignment
+            {
+                // Title of the assignment
+                Title = "Default Title" // Required by annotation
+            };
+            Assert.NotNull(assignment.Title);
+        }
+    }
+}
